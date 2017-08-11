86d4f1b81dde3daf197bed75c26222e41c2d838f
Vinicius S. Pereira <struginskipereira14@hotmail.com>
2017-08-11 10:53:57 -0300

    Icones NFC

--- sendemail.php
+++ sendemail.php
@@ -16,9 +16,10 @@ $message = strip_tags(htmlspecialchars($_POST['message']));
 // Create the email and send the message
 $to = 'contato@microvil.com.br'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Contato pelo site de: $name";
-$email_body = "Você recebeu uma nova mensagem através do formulário de contato do site.\n\n"."Aqui estão os detalhes:\n\nNome: $name\n\nE-mail: $email_address\n\nTelefone: $phone\n\nMensagem:\n$message";
+$email_body = "Você recebeu uma nova mensagem através do formulário de contato do site.\n\n"."Aqui estão os detalhes:\n\nNome: $name\n\nE-mail: $email_address\n\nMensagem:\n$message";
 $headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";   
+var_dump(1);
 mail($to,$email_subject,$email_body,$headers);
 return true;         
 ?>
